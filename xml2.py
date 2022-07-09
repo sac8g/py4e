c6ef4d1fb4b79d26ca39296ace5517523aab6b51
sac8g <braverman.igor@gmail.com>
2022-07-08 22:49:42 -0400

    Daily updates 2022/07/08

---
+++ xml2.py
@@ -0,0 +1,34 @@
+import xml.etree.ElementTree as ET
+
+input = '''
+<stuff>
+  <users>
+    <user x="2">
+      <id>001</id>
+      <name>Chuck</name>
+    </user>
+    <user x="7">
+      <id>009</id>
+      <name>Brent</name>
+    </user>
+  </users>
+</stuff>'''
+
+stuff = ET.fromstring(input)
+lst = stuff.findall('users/user')
+print(lst)
+print('User count:', len(lst))
+
+for item in lst:
+    print('Name', item.find('name').text)
+    print('Id', item.find('id').text)
+    print('Attribute', item.get('x'))
+
+# [<Element 'user' at 0x000002318D1E4CC0>, <Element 'user' at 0x000002318D1E4DB0>]
+# User count: 2
+# Name Chuck
+# Id 001
+# Attribute 2
+# Name Brent
+# Id 009
+# Attribute 7
